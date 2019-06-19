92ecc2467db97bc51788d424a85f908bff103820
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2019-06-19 16:14:36 +0200

    fix(movies/search): modify some imports

--- components/movies/search/src/component.js
+++ components/movies/search/src/component.js
@@ -1,46 +1,43 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import AtomInput from '@s-ui/react-atom-input'
-import AtomCard from '@s-ui/react-atom-card'
+import MoviesSearchBox from 'mv-movies-search-box'
+import MoviesSearchList from 'mv-movies-search-list'
 
 class MoviesSearch extends Component {
   state = {
-    list: []
+    list: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true})
+    const {list} = await this.props.domain
+      .get('get_trending_movies_use_case')
+      .execute()
+
+    this.setState({list, loading: false})
   }
 
   _onSearch = async (_, {value}) => {
+    this.setState({loading: true})
     const {list} = await this.props.domain
       .get('search_movies_use_case')
       .execute({
         keyword: value
       })
 
-    this.setState({list})
+    this.setState({list, loading: false})
   }
 
   render() {
     return (
       <div className="mv-MoviesSearch">
-        <div className="mv-MoviesSearch-box">
-          <AtomInput type="text" onEnter={this._onSearch} />
-        </div>
-        <div className="mv-MoviesSearch-list">
-          {this.state.list.map(item => {
-            const imageSrc = `https://image.tmdb.org/t/p/w200${item.image}`
-            const renderMedia = () => (
-              <img className="mv-MoviesSearch-itemImage" src={imageSrc} />
-            )
-            const renderContent = () => <header>{item.title}</header>
-
-            return (
-              <AtomCard
-                key={item.id}
-                media={renderMedia}
-                content={renderContent}
-              />
-            )
-          })}
-        </div>
+        <MoviesSearchBox onSearch={this._onSearch} />
+        {this.state.loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <MoviesSearchList list={this.state.list} />
+        )}
       </div>
     )
   }
